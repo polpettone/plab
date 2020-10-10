c746d0682bc5b1bb7cd6735c6f6264501992bb26
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-10-10 22:09:36 +0200

    refactoring

--- cmd/fileScanner.go
+++ cmd/fileScanner.go
@@ -15,28 +15,46 @@ type File struct {
 	info os.FileInfo
 }
 
-func(fileScanner FileScanner) list(path string) {
+type FileListResult struct {
+	Files []File
+	StartTime time.Time
+	EndTime time.Time
+}
 
-	fileScanner.Logging.Stdout.Printf("List Files")
-
+func listFiles(path string) (*FileListResult, error) {
 	startTime := time.Now()
-
 	var files []File
 	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		files = append(files, File{path, info})
 		return nil
 	})
 	endTime := time.Now()
-	duration := endTime.Sub(startTime)
 
-	fileScanner.Logging.Stdout.Printf("Scanned %d Files in %d ms", len(files), duration * time.Millisecond)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileListResult{
+		files,
+		startTime,
+		endTime,
+	}, nil
+}
+
+func(fileScanner FileScanner) list(path string) {
+	fileScanner.Logging.Stdout.Printf("List Files")
+
+	fileListResult, err := listFiles(path)
 
 	if err != nil {
 		fileScanner.Logging.ErrorLog.Printf("%v", err)
+		return
 	}
 
-	for _, file := range files {
+	duration := fileListResult.EndTime.Sub(fileListResult.StartTime).Milliseconds()
+	fileScanner.Logging.Stdout.Printf("Scanned %d Files in %d ms", len(fileListResult.Files), duration)
+
+	for _, file := range fileListResult.Files {
 		fileScanner.Logging.InfoLog.Printf("%s %d", file.path, file.info.Size())
 	}
-
 }
