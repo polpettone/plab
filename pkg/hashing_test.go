52db8546fc4109991d9fdc050eb1407d7a7e262a
Stefan Heiden <stefan.heiden@qudosoft.de>
2020-11-12 17:13:38 +0100

    understanding brypt

--- pkg/hashing_test.go
+++ pkg/hashing_test.go
@@ -7,6 +7,15 @@ import (
 	"time"
 )
 
+func Test_should_match(t *testing.T) {
+	//value := "$2a$10$oryrXDxtlYTflG6aFMoWoOk6JCx7qlW832SmdRx2KjJwTHsmonWVe"
+	value := "$2a$10$zw3/lkIm1MgE0lutWYkbiul/Ni5qSkPquaS6D3LHi3PlOVDYvuhVy"
+	err := bcrypt.CompareHashAndPassword([]byte(value), []byte("123456"))
+
+	if err != nil {
+		t.Errorf("Not matched")
+	}
+}
 
 //Two calls on the same value produces different outputs
 func Test_bcryptHash_result_is_not_equalt(t *testing.T){
@@ -27,41 +36,29 @@ func Test_bcryptHash_should_successful_compare_passwords(t *testing.T) {
 	}
 }
 
-func Test_bcryptHash_show_hashing_execution_time_with_different_costs(t *testing.T) {
-
-	value := "secret"
-
+func hashAndCompareCostDurationComparison(plainPassword string, cost int) (time.Duration, time.Duration) {
 	start := time.Now()
-	bcrypt.GenerateFromPassword([]byte(value), 4)
+	hash, _ := bcrypt.GenerateFromPassword([]byte(plainPassword), cost)
 	end := time.Now()
-	duration := end.Sub(start)
-	fmt.Printf("Cost 4 Duration %d\n", duration.Milliseconds())
-
+	hashDuration := end.Sub(start)
 
 	start = time.Now()
-	bcrypt.GenerateFromPassword([]byte(value), 10)
+	bcrypt.CompareHashAndPassword(hash, []byte(plainPassword))
 	end = time.Now()
-	duration = end.Sub(start)
-	fmt.Printf("Cost 10 Duration %d\n", duration.Milliseconds())
+	compareDuration := end.Sub(start)
 
-	start = time.Now()
-	bcrypt.GenerateFromPassword([]byte(value), 14)
-	end = time.Now()
-	duration = end.Sub(start)
-	fmt.Printf("Cost 14 Duration %d\n", duration.Milliseconds())
+	return hashDuration, compareDuration
+}
 
-	start = time.Now()
-	bcrypt.GenerateFromPassword([]byte(value), 15)
-	end = time.Now()
-	duration = end.Sub(start)
-	fmt.Printf("Cost 15 Duration %d\n", duration.Milliseconds())
-
-	start = time.Now()
-	bcrypt.GenerateFromPassword([]byte(value), 16)
-	end = time.Now()
-	duration = end.Sub(start)
-	fmt.Printf("Cost 16 Duration %d\n", duration.Milliseconds())
-
+func Test_bcryptHash_show_hashing_execution_time_with_different_costs(t *testing.T) {
+	passwords := []string{"secret", "123456", "skldh__!ksdhg03485789nfvsd!31"}
+	costs:= []int{1, 10, 14, 15 ,16}
+	for _, cost := range costs {
+		for _, password := range passwords {
+			hashDuration, compareDuration := hashAndCompareCostDurationComparison(password, cost)
+			fmt.Printf("%d %v %v %s\n", cost, hashDuration, compareDuration, password)
+		}
+	}
 }
 
 
